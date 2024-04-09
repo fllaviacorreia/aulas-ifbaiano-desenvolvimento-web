be57f733adbf1a1711ede8b79a2ce5df3a88eab8
Flávia Correia <fllaviacorreia@outlook.com>
2024-04-09 18:13:47 -0300

    alters

--- code/intellij/payment-api/src/main/java/com/project/payment/domain/model/ClienteModel.java
+++ code/intellij/payment-api/src/main/java/com/project/payment/domain/model/ClienteModel.java
@@ -3,8 +3,8 @@ package com.project.payment.domain.model;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
+
+import java.util.Objects;
 
 @Data
 @Entity
@@ -14,7 +14,7 @@ public class ClienteModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
+    private String nome;
     private String email;
     private String phone;
 }
--- code/intellij/payment-api/src/main/java/com/project/payment/domain/repository/ClienteRepository.java
+++ code/intellij/payment-api/src/main/java/com/project/payment/domain/repository/ClienteRepository.java
@@ -1,2 +1,13 @@
-package com.project.payment.domain.repository;public interface ClienteRepository {
+package com.project.payment.domain.repository;
+
+import com.project.payment.domain.model.ClienteModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+    List<ClienteModel> findByNome(String nome);
+    List<ClienteModel> findByNomeContains(String nome);
 }
