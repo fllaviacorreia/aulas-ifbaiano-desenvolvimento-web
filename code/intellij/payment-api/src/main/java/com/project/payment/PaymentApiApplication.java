7d05c2ae5aa65016d60fbe93e5da95034da1b0cf
Flávia Correia <fllaviacorreia@outlook.com>
2024-04-09 15:50:05 -0300

    include files

--- code/intellij/payment-api/src/main/java/com/project/payment/PaymentApiApplication.java
+++ code/intellij/payment-api/src/main/java/com/project/payment/PaymentApiApplication.java
@@ -1,9 +1,27 @@
 package com.project.payment;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 class PaymentApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty(
+				"spring.datasource.url",
+				dotenv.get("DB_URL")
+		);
+		System.setProperty(
+				"spring.datasource.username",
+				dotenv.get("DB_USER")
+		);
+		System.setProperty(
+				"spring.datasource.password",
+				dotenv.get("DB_PASSWORD")
+		);
+
 		SpringApplication.run(PaymentApiApplication.class, args);
 	}
 
--- code/intellij/payment-api/src/main/java/com/project/payment/domain/model/ClienteModel.java
+++ code/intellij/payment-api/src/main/java/com/project/payment/domain/model/ClienteModel.java
@@ -1,11 +1,18 @@
 package com.project.payment.domain.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter
-@Setter
+@Data
+@Entity
+@Table(name="client")
 public class ClienteModel {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
