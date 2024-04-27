96fcd17154a6c01af0fc670cae71611f11936479
Flávia Correia <fllaviacorreia@outlook.com>
2024-04-27 19:50:11 -0300

    up

--- code/ss/payment-api/src/main/java/com/project/payment/domain/model/ClienteModel.java
+++ code/ss/payment-api/src/main/java/com/project/payment/domain/model/ClienteModel.java
@@ -7,7 +7,7 @@ import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-@Data
+@Data 
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name="client")
---
+++ code/ss/payment-api/src/main/java/com/project/payment/domain/service/GerenciamentoParcelamentoService.java
@@ -0,0 +1,2 @@
+package com.project.payment.domain.service;public class GerenciamentoParcelamentoService {
+}
